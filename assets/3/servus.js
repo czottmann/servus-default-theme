03e67093681b18c06ee9eb46cedcc6c3238fe5d2
Carlo Zottmann <carlo@municode.de>
2013-01-18 19:34:47 +0100

    [CHG] Moved sharing button right next to download button.  [NEW] Better display on small screens.

--- assets/3/servus.js
+++ assets/3/servus.js
@@ -1,24 +1,57 @@
+/*jshint asi: false, bitwise: false, boss: false, browser: true,
+  camelcase: true, curly: true, debug: false, devel: true, eqeqeq: true,
+  eqnull: false, evil: false, forin: false, immed: true, indent: 4,
+  laxbreak: true, newcap: true, noarg: true, noempty: false, nomen: false,
+  nonew: false, onevar: false, passfail: false, plusplus: false,
+  quotmark: 'double', regexp: false, shadow: false, strict: false, sub: true,
+  trailing: true, undef: true, white: false */
+/*global window: false, $: false, _: false */
+
+
 var S = window.S || {};
 
+// The resize handler.
 S.onResize = function(evt) {
+  var viewportHeight = $(window).height();
+
+  $("#download-details .img-rounded").css(
+    "max-height",
+    Math.floor( viewportHeight - S.nominalHeightBox - 20 )
+  );
+
   $("#main-content").css(
     "padding-top",
-    Math.floor( ( $(window).height() - $("#shared-file").height() ) / 2 )
+    Math.floor( ( viewportHeight - $("#shared-file").height() ) / 2 )
   );
 };
 
+
+// Attach resize handler.
 $(window)
   .on( "resize", S.onResize )
   .on( "load", S.onResize );
 
+
 $(document).ready( function() {
   S.pageTitle = $.trim( $(".popover-title").text() );
+  S.nominalHeightBox = $("#shared-file").height() - $("#download-details .img-rounded").height();
+  S.onResize();
 
   // Reset the details box position when the file is image and it has finished
   // loading.  Also, mark the box as containing an image.
   $("#download-details").filter(":has(.img-rounded)")
     .addClass("type-image")
     .find(".img-rounded").on( "load", S.onResize );
+
+  // Show "Shared using Servus for Mac." on mouse hovering over sharing
+  // button.
+  $("#sharing, #shared-by")
+    .on( "mouseenter", function() {
+      $("#shared-by").addClass("s-visible").css( "visibility", "visible" );
+    })
+    .on( "mouseleave", function() {
+      $("#shared-by").removeClass("s-visible");
+    });
 
   // Generate Twitter sharing link and attach click handler
   $(".share-twitter")
